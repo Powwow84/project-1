196c97ce07600220cc324204381e013cf30603fb
Powwow84 <pao.saelee84@gmail.com>
2023-03-17 16:33:28 -0700

    added hero and movement

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,59 @@ function renderMaze(ctx, mazeArray) {
   }
   
   renderMaze(ctx, mazeArray)
+
+
+class Crawler {
+    constructor(x, y, width, height, color) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.color = color
+        this.alive = true
+    }
+
+    render() {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+
+document.addEventListener('keydown', handleKeyPressEvent)
+
+// game objects
+
+const hero = new Crawler(10, 10, 10, 10, 'blue')
+hero.render()
+
+
+function handleKeyPressEvent(e) {
+    const speed = 10
+    let prevX = hero.x;
+    let prevY = hero.y;
+
+    switch(e.key) {
+        case "w":
+        case "ArrowUp" :
+            hero.y -= speed
+            break;
+        case "s" :
+        case "ArrowDown" :
+            hero.y += speed
+            break;
+         case "a" :   
+         case "ArrowLeft" :
+            hero.x -= speed
+            break;
+         case "d":   
+         case "ArrowRight" :
+            hero.x += speed
+            break; 
+    }
+    movement.innerText = `x: ${hero.x} y: ${hero.y}`
+    ctx.fillStyle = '#F7F1E5';
+    ctx.fillRect(prevX, prevY, hero.width, hero.height);
+    hero.render()
+}
+
